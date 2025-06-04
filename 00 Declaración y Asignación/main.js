32e6b29b2e3a01e5a196ed204a627d9c0d3f2b36
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-06-04 18:02:59 -0300

    6to DWD

---
+++ 00 Declaración y Asignación/main.js
@@ -0,0 +1,27 @@
+/* 
+
+    Declaración
+    En JavaScript, pueden declarar variables usando:
+
+        - var (forma antigua, ya no se recomienda)
+
+        - let (forma moderna, permite reasignación)
+
+        - const (forma moderna, no permite reasignación)
+
+
+*/
+
+// ---> Ejemplo: 
+let Nombre = "Pablo"
+let Edad=30
+
+/* 
+    En let Nombre --> estás diciendo que existe una variable llamada Nombre.
+     
+    = --> Se utiliza para Asignar un dato, en este caso "Pablo" 
+
+    "" --> Se utiliza para Indicar que el valor a almacenar es tipo String
+    
+    Los Valores de tipo númerico (Int, Float...) se asignan sin comillas
+*/
