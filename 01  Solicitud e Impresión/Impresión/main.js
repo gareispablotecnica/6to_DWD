f8bdd9cd7db6ac218a360babc98562b419c78b5b
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-06-04 18:27:26 -0300

    6to DWD

---
+++ 01  Solicitud e Impresión/Impresión/main.js
@@ -0,0 +1,45 @@
+/*
+
+    *console.log()
+        Sirve para mostrar información en la consola del navegador, generalmente usado para depurar (ver valores, probar código, etc.).
+
+        console.log()
+
+    * Alert
+        Muestra una ventana emergente  con un mensaje. Es útil para informar al usuario.
+
+        alert()
+
+*/
+
+// ---> Ejemplo 01
+
+let Apellido ="Gareis"
+console.log("El Apellido es: ",Apellido)
+
+/*
+    - "El Apellido es: " --> Es un String Informativo
+    
+    - , --> La "Coma" , Se utiliza para concatenar Variables
+
+    - Apellido --> Es la variable que almacena un Dato
+
+    - Para Verificar la Impresión --> Hacemos Clic derecho sobre la pagina web -->
+      Inspeccionar --> console.
+*/
+
+
+// ---> Ejemplo 02
+
+let Edad =30
+alert("El Edad es: "+Edad)
+
+/*
+    - "El Edad es: " --> Es un String Informativo
+    
+    - + --> La "Coma" , Se utiliza para concatenar Variables
+
+    - Edad --> Es la variable que almacena un Dato
+
+    - Para Verificar la Impresión --> Solo actualizar el navegador.
+*/
---
+++ 01  Solicitud e Impresión/Solicitud/main.js
@@ -0,0 +1,45 @@
+/*
+
+    *prompt()
+        Sirve para solicitar mediante un INPUT un dato al Usuario.
+
+        prompt("Ingrese su Nombre: ")
+
+        Captura datos tipo String
+
+    * parseInt o parseFloat
+        parseInt --> Convierte un String en un Entero
+        -parseInt(prompt("Ingrese su Edad: "))
+        
+        parseFloat --> Convierte un String en un Decimal
+        -parseFloat(prompt("Ingrese su Sueldo: "))
+*/
+
+
+// ---> Solicitamos el Ingreso del nombre del Usuario
+
+let nombre =prompt("Ingrese su Nombre: ")
+
+/*
+    - nombre --> Variable que almacena el Dato ingresado por el Usuario
+
+    - let ---> Asignacion de Variable
+
+    - prompt ("Ingrese su Nombre: ") --> Solicitud por Pantalla
+
+*/
+
+// ---> Solicitamos el Ingreso de la Edad del Usuario
+
+let edad = parseInt(prompt("Ingrese su Edad: "))
+
+/*
+    - edad --> Variable que almacena el Dato ingresado por el Usuario
+
+    - let ---> Asignacion de Variable
+
+    - prompt("Ingrese su Edad: ") --> Solicitud por Pantalla
+
+    - parseInt --> Convierte el Valor que Ingresa el Usuario en un Valor Entero.
+
+*/
